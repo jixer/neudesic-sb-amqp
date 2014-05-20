43722f550cd100b3b6f86f400f98dc2304fe7726
Chris' Mac <chris@Chriss-MacBook-Pro.local>
2014-05-19 22:55:11 -0700

    XCode project working now

--- SRC/C++/XCode/amqp.poc/AmqpWrapper.cpp
+++ SRC/C++/XCode/amqp.poc/AmqpWrapper.cpp
@@ -17,28 +17,65 @@
 #include <ctype.h>
 
 AmqpWrapper::AmqpWrapper() {
-    amqpAddress = (char *) "amqp://0.0.0.0";
+    amqpAddress = (char *) "amqps://<user_name>:<password>@neudesic-amqp-demo.servicebus.windows.net/amqp_test";
 }
 
 void AmqpWrapper::SendMessage(char * msgtext) {
-    
     pn_message_t * message;
     pn_messenger_t * messenger;
     pn_data_t * body;
+    pn_data_t * props;
+    
+    int err = 0;
         
     message = pn_message();
     messenger = pn_messenger(NULL);
         
     pn_messenger_start(messenger);
+    configureAzureSbMessage(message, "Test message from Mac");
     
-    pn_message_set_address(message, amqpAddress);
     body = pn_message_body(message);
     pn_data_put_string(body, pn_bytes(strlen(msgtext), msgtext));
-    pn_messenger_put(messenger, message);
     
-    pn_messenger_send(messenger, 0);
+    props = setProperties(message, (char *)"TestProp text value", 10);
+    
+    err = pn_messenger_put(messenger, message);
+    if (err != 0) {
+        printf("put error code: %d\r\n", err);
+    }
+    
+    
+    err = pn_messenger_send(messenger, -1);
+    if (err != 0) {
+        printf("send error code: %d\r\n", err);
+    }
     
     pn_messenger_stop(messenger);
     pn_messenger_free(messenger);
     pn_message_free(message);
+}
+
+pn_data_t * AmqpWrapper::setProperties(pn_message_t *message, char *stringProp, int intProp) {
+    // initialize
+    pn_data_t * props = pn_message_properties(message);
+    pn_data_put_map(props);
+    pn_data_enter(props);
+    
+    // set prop1
+    pn_data_put_string(props, pn_bytes(9, (char *)"TestProp1"));
+    pn_data_put_string(props, pn_bytes(strlen(stringProp), stringProp));
+    
+    // set prop2
+    pn_data_put_string(props, pn_bytes(9, (char *)"TestProp2"));
+    pn_data_put_int(props, intProp);
+    
+    // exit and return
+    pn_data_exit(props);
+    return props;
+}
+
+void AmqpWrapper::configureAzureSbMessage(pn_message_t *message, const char *label) {
+    pn_message_set_subject(message, label);
+    pn_message_set_ttl(message, PN_TTL);
+    pn_message_set_address(message, amqpAddress);
 }
--- SRC/C++/XCode/amqp.poc/AmqpWrapper.h
+++ SRC/C++/XCode/amqp.poc/AmqpWrapper.h
@@ -10,6 +10,7 @@
 #define __amqp_poc__AmqpWrapper__
 
 #include <iostream>
+#include "proton/message.h"
 
 #endif /* defined(__amqp_poc__AmqpWrapper__) */
 
@@ -19,4 +20,7 @@ public:
     void SendMessage(char* message);
 private:
     char * amqpAddress;
+    const pn_millis_t PN_TTL = 86400000;
+    pn_data_t * setProperties(pn_message_t * message, char * stringProp, int intProp);
+    void configureAzureSbMessage(pn_message_t * message, const char * label);
 };
